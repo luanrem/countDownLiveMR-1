3990f489bb1bc8007940276c06898d15c9dec7cc
Luan Martins <luanrem@gmail.com>
2020-05-07 16:10:33 -0300

    fix error

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" component={Main}/>
+        <Route path="/" component={Main} host="https://contagemrama.herokuapp.com/"/>
       </Switch>
     </BrowserRouter>
   );
